fedcaf45b692539e75b658435317aca8063899c8
shiaujib <ch23173885@yahoo.com.tw>
2016-12-14 17:49:18 +0000

    debug

--- main.py
+++ main.py
@@ -33,24 +33,24 @@ def main(argv):
     except getopt.GetoptError:
         print 'usage:muti_accelerometer.py -n <deviceNumber> -s <subjectName>'
         sys.exit(2)
+    '''
     if findElement(argv,'-n')==0:
         print 'usage:muti_accelerometer.py -n <deviceNumber> -s <subjectName>'
         sys.exit(2)
     if findElement(argv,'-s')==0:
         print 'usage:muti_accelerometer.py -n <deviceNumber> -s <subjectName>'
         sys.exit(2)
+    '''
     for opt,arg in opts:
         if opts=='-h':
             print 'usage:muti_accelerometer.py -i <deviceNumber>'
         elif opt in ("-n","--deviceNumber"):
             deviceNum=arg
-    elif opt in ("-s","--subjectName"):
-        subName=arg
+    	elif opt in ("-s","--subjectName"):
+            subName=arg
 
-    accel=[[] for i in range(int(deviceNum))]  #create dynamic list
-    gyro=[[] for i in range(int(deviceNum))]
-    fileName=[subName+'sensor1.txt',subname+'sensor2.txt']
-
+    fileName=[subName+'_sensor1.txt',subName+'_sensor2.txt']
+    count=0
     print ""
     print "Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S")
     print ""
@@ -66,12 +66,11 @@ def main(argv):
     fileList=[file0,file1]
     accel_tmp=[0]*int(deviceNum)
     start=time.time()
+    print "getting data please press button to stop........"
     while True:
         fileIndex=0
         input_state=GPIO.input(4)   #get switch state
-    print "getting data please press button to stop........"
         mpu6050 = MPU6050Read.MPU6050Read(0x68,1)
-        tca9548.write_control_register(BusChannel[fileIndex])
         #get gyro and accelerometer value
         gyro_xout = mpu6050.read_word_2c(0x43)
         gyro_yout = mpu6050.read_word_2c(0x45)
@@ -79,12 +78,12 @@ def main(argv):
         accel_xout = mpu6050.read_word_2c(0x3b)
         accel_yout = mpu6050.read_word_2c(0x3d)
         accel_zout = mpu6050.read_word_2c(0x3f)
-        accel_xout=accel_xout/16384
-        accel_yout=accel_yout/16384
-        accel_zout=accel_zout/16384
-        gyro_xout=gyro_xout/131
-        gyro_yout=gyro_yout/131
-        gyro_zout=gyro_zout/131
+        accel_xout=accel_xout/16384.0
+        accel_yout=accel_yout/16384.0
+        accel_zout=accel_zout/16384.0
+        gyro_xout=gyro_xout/131.0
+        gyro_yout=gyro_yout/131.0
+        gyro_zout=gyro_zout/131.0
         end=time.time()
         realtime=end-start
         fileList[fileIndex].write("%f\t%f\t%f\t%f\t%f\t%f\t%f\n" %(accel_xout,accel_yout,accel_zout,gyro_xout,gyro_yout,gyro_zout,realtime))
@@ -96,20 +95,20 @@ def main(argv):
         accel_xout = mpu6050_sla.read_word_2c(0x3b)
         accel_yout = mpu6050_sla.read_word_2c(0x3d)
         accel_zout = mpu6050_sla.read_word_2c(0x3f)
-        accel_xout=accel_xout/16384
-        accel_yout=accel_yout/16384
-        accel_zout=accel_zout/16384
-        gyro_xout=gyro_xout/131
-        gyro_yout=gyro_yout/131
-        gyro_zout=gyro_zout/131
+        accel_xout=accel_xout/16384.0
+        accel_yout=accel_yout/16384.0
+        accel_zout=accel_zout/16384.0
+        gyro_xout=gyro_xout/131.0
+        gyro_yout=gyro_yout/131.0
+        gyro_zout=gyro_zout/131.0
         end=time.time()
         realtime=end-start
-        fileList[fileIndex].write("%f\t%f\t%f\t%f\t%f\t%f\t%f\n" %(accel_xout,accel_yout,accel_zout,gyro_xout,gyro_yout,gyro_zout,realtime))
+        fileList[fileIndex+1].write("%f\t%f\t%f\t%f\t%f\t%f\t%f\n" %(accel_xout,accel_yout,accel_zout,gyro_xout,gyro_yout,gyro_zout,realtime))
         count+=1
         if input_state==False:
             ttime=time.time()-start
             print "Button Pressed experimental stop"
-            print "Total time : %d Amount of data = %f" %ttime
+            print "Total time : %f" %ttime
             print "Amount of data = %d" %count
             sys.exit()
             break 
